e752daf1356fab1257892c5cd0b4f922480f8a84
N U R O ™ <me@nuro.dev>
2021-01-02 22:59:22 +0000

    Renamed component header to hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,9 +1,9 @@
 <template>
-	<header class="h-screen">
+	<div class="h-screen">
 		<Background />
-		<div class="absolute inset-0 text-center">
+		<div class="absolute inset-0 text-center flex flex-col justify-center">
 			<img alt="Vue logo" class="mx-auto" src="../assets/logo.png" />
 			<HelloWorld msg="Hello Vue 3.0 + Vite" />
 		</div>
-	</header>
+	</div>
 </template>
