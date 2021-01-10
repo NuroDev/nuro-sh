39ae449b3e468dea0fab6eb44303c1219d8b3312
N U R O ™ <me@nuro.dev>
2021-01-10 08:25:23 +0000

    Added WIP profile card

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,9 +1,27 @@
 <template>
 	<div class="h-screen">
 		<Background />
-		<div class="absolute inset-0 text-center flex flex-col justify-center">
-			<img alt="Vue logo" class="mx-auto" src="../assets/logo.png" />
-			<HelloWorld msg="Hello Vue 3.0 + Vite" />
+		<div class="absolute inset-0 min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+			<div class="max-w-5xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+				<div class="bg-blue-700 ring-4 ring-blue-700 ring-opacity-50 rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4">
+					<div class="pt-10 pb-12 px-6 sm:pt-16 sm:px-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20">
+						<div class="lg:self-center">
+							<h2 class="text-3xl font-extrabold text-white sm:text-4xl">
+								<span class="block">Ready to dive in?</span>
+								<span class="block">Start your free trial today.</span>
+							</h2>
+
+							<p class="mt-4 text-lg leading-6 text-blue-200">Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla nec.</p>
+
+							<a href="#" class="mt-8 bg-white border border-transparent rounded-md shadow px-6 py-3 inline-flex items-center text-base font-medium text-blue-600 hover:bg-blue-50">Sign up for free</a>
+						</div>
+					</div>
+
+					<div class="-mt-6 aspect-w-5 aspect-h-3 md:aspect-w-2 md:aspect-h-1">
+						<img class="transform translate-x-6 translate-y-6 rounded-md object-cover object-left-top sm:translate-x-16 lg:translate-y-20" src="https://tailwindui.com/img/component-images/full-width-with-sidebar.jpg" alt="App screenshot">
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
