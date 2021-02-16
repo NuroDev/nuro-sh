2d62503ec5bb8a42ea2cd894d433c59247b873ea
nurodev <me@nuro.dev>
2021-02-16 11:29:19 +0000

    Switched to dynamically import SVG components

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import { VitePWA as PWA } from 'vite-plugin-pwa';
 import Components from 'vite-plugin-components';
+import Icons, { ViteIconsResolver } from 'vite-plugin-icons';
 import Markdown from 'vite-plugin-md';
 import Pages from 'vite-plugin-pages';
 import SVG from 'vite-plugin-svg';
@@ -11,7 +12,17 @@ const extensions: Array<string> = ['vue', 'js', 'ts', 'md'];
 
 export default defineConfig({
 	plugins: [
-		Components({ extensions }),
+		Components({
+			extensions,
+			customLoaderMatcher: id => id.endsWith('.md'),
+			customComponentResolvers: [
+				ViteIconsResolver({
+					componentPrefix: '',
+					enabledCollections: ['feather'],
+				}),
+			],
+		}),
+		Icons(),
 		Markdown(),
 		Pages({ extensions }),
 		PWA({
@@ -37,7 +48,7 @@ export default defineConfig({
 		Vue({
 			include: [/\.vue$/, /\.md$/],
 		}),
-		WindiCSS({
+		...WindiCSS({
 			windicssOptions: {
 				darkMode: 'class',
 				plugins: [require('windicss/plugin/aspect-ratio')],
